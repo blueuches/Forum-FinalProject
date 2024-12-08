0113bddce2f7678cce33fcb8af44d1780e77872d
blueuches <dengrcmrn@gmail.com>
2024-12-08 12:14:55 +0800

    show post

---
+++ resources/js/Components/PostCard.vue
@@ -0,0 +1,41 @@
+<template>
+    <div class="p-6 max-w-4xl flex bg-white rounded-lg border border-gray-200 shadow-md dark:bg-gray-800">
+        <div class="mr-3">upvote</div>
+        <div>
+            <div class="flex m-2 p-2">
+            <span class="font-semibold mr-3">r/{{ community }}</span>
+            <div class="flex">Posted by 
+                <span class="font-semibold ml-1">{{ post.username }}</span>
+            </div>
+            </div>
+            <a href="#">
+            <h5 class="mb-2 text-2xl font-bold tracking-tight text-gray-900 dark:text-white">
+                {{ post.title }}
+            </h5>
+            </a>
+
+            <p class="mb-3 font-normal text-gray-700 dark:text-gray-400">
+            {{post.description}}
+            </p>
+
+            <div class="flex m-2 p-2">
+            <p class="mr-4 p-2">Comments(2)</p>
+            <a href="#" class="inline-flex items-center py-2 px-4 text-sm font-medium text-white bg-blue-600 rounded-lg hover:bg-blue-700 focus:ring-4 focus:outline-none focus:ring-blue-300 dark:bg-blue-500 dark:hover:bg-blue-600 dark:focus:ring-blue-800">
+                Read more
+                <svg aria-hidden="true" class="ml-2 -mr-1 w-4 h-4" fill="currentColor" viewBox="0 0 20 20" xmlns="http://www.w3.org/2000/svg">
+                <path fill-rule="evenodd" d="M10 3a1 1 0 01.832.445l5 7a1 1 0 01-1.664 1.11L10 5.778 5.832 11.555a1 1 0 01-1.664-1.11l5-7A1 1 0 0110 3zm0 10a1 1 0 00-.832.445l-5 7a1 1 0 001.664 1.11L10 14.222l4.168 5.333a1 1 0 001.664-1.11l-5-7A1 1 0 0010 13z" clip-rule="evenodd"></path>
+                </svg>
+            </a>
+            </div>
+        </div>
+    </div>
+
+</template>
+
+<script setup>
+
+defineProps({
+    post: Object,
+    community: String
+})
+</script>
--- resources/js/Pages/Frontend/Communities/Show.vue
+++ resources/js/Pages/Frontend/Communities/Show.vue
@@ -1,17 +1,28 @@
 <template>
 <GuestLayout>
-    <div class="flex justify-end m-2 p-2">
-        <Link v-if="$page.props.auth.auth_check" :href="route('communities.posts.create', community.slug)" class="px-3 py-4 rounded bg-indigo-500">Create Post</Link>
-    </div>
-    <h1>Community  {{ community.name }} show page</h1>
+    <template #header>
+            <h2
+                class="text-xl font-semibold leading-tight text-gray-800"
+            >
+                r/{{ community.name }}
+            </h2>
+            <div class="flex justify-end m-2 p-2">
+                <Link v-if="$page.props.auth.auth_check" :href="route('communities.posts.create', community.slug)" class="px-3 py-4 rounded bg-indigo-500">Create Post</Link>
+            </div>
+        </template>
+    <section  class="m-2 p-2">
+        <PostCard v-for="post in posts.data" :post="post" :community="community.name" :key="post.id"/>
+    </section>
 </GuestLayout>
 </template>
 
 <script  setup>
 import GuestLayout from "@/Layouts/GuestLayout.vue";
 import {Link } from "@inertiajs/vue3";
+import PostCard from "@/Components/PostCard.vue";
 
 const props = defineProps({
-    community: Object(),
+    community: Object,
+    posts: Object,
 })
 </script>
